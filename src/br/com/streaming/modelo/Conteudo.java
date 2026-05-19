66a337e28a416295eec354d6fcf77f27b56e3b20
jacksonbjunior-star <jackson.b.junior@ba.estudante.senai.br>
2026-05-19 01:22:24 -0300

    feat: implementando classe abstrata Conteudo com validacoes

---
+++ src/br/com/streaming/modelo/Conteudo.java
@@ -0,0 +1,43 @@
+package br.com.streaming.modelo;
+
+public abstract class Conteudo {
+    private String titulo;
+    private int duracao; //em minutos
+    private int classificacaoIndicativa; //em anos
+
+    public Conteudo(String titulo, int duracao, int classficacaoIndicativa) {
+        this.titulo = titulo;
+        setDuracao(duracao);
+        setClassificacaoIndicativa(classficacaoIndicativa);
+    }
+
+    public abstract void exibirDetalhes();
+
+    public abstract void reproduzir() {
+        System.out.println("Reproduzindo agora: " + this.titulo);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setDuracao(int duracao) {
+        if (duracao > 0) {
+            this.duracao = duracao;
+        } else {
+            System.out.println("Erro. Duração deve ser maior que zero.");
+        }
+    }
+
+    public int getClassificacaoIndicativa() {
+        return classificacaoIndicativa;
+    }
+    public void setClassificacaoIndicativa(int classificacaoIndicativa) {
+        if (classificacaoIndicativa >= 0) {
+            this.classificacaoIndicativa = classificacaoIndicativa;
+        } else {
+            System.out.println("Erro. Classificação indicativa deve ser maior ou igual a zero.");
+        }
+    }
+}
+
